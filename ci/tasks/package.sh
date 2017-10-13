2dc9738fc11a4c256f9276cc62a392c8a193008e
C <238717@AMB00679.local>
2017-10-13 15:47:20 +0530

    package.sh with additional echos

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -2,19 +2,22 @@
 #!/bin/bash
 
 set -e +x
-
+ echo "About to build"
 pushd movie-fun
   echo "Packaging WAR"
   ./mvn clean package -DskipTests -Dmaven.test.skip=true
 popd
+echo "Building done"
+
 
 jar_count=`find movie-fun/target -type f -name *.war | wc -l`
+echo " Found the WAR"
 
 if [ $jar_count -gt 1 ]; then
   echo "More than one war found, don't know which one to deploy. Exiting"
   exit 1
 fi
-
+echo "move the file to package output"
 find movie-fun/target -type f -name *.war -exec cp "{}" package-output/moviefun.war \;
 
 echo "Done packaging"
